c8a302a756e7c3bd9b502e4a5da1962f6c310aa7
outputonlypunch2020xx <outputonlypunch2020xx@gmail.com>
2020-09-30 11:40:02 +0900

    メモ投稿機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   end
   def create
     Post.create(content: params[:content])
-    redirect_to action: :index
+    redirect_to action: :index   
   end
   def checked
    
